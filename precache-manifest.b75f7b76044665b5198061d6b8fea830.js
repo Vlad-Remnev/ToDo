1bd1e9ddb8946ee70368355af9a8b763297e39f1
Vladislav Remnev <remnevvlad30@gmail.com>
2022-07-22 14:59:15 +0300

    Updates

---
+++ precache-manifest.b75f7b76044665b5198061d6b8fea830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a088513c6ccfebefbc4129c5ce77eafd",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "5f915b1d71cd4ec1d883",
+    "url": "/ToDo/static/css/main.96dc9a71.chunk.css"
+  },
+  {
+    "revision": "d6a68ba395e88b4be620",
+    "url": "/ToDo/static/js/2.395a81a7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ToDo/static/js/2.395a81a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f915b1d71cd4ec1d883",
+    "url": "/ToDo/static/js/main.197a37c1.chunk.js"
+  },
+  {
+    "revision": "17bdeea83c1f59c3c01e",
+    "url": "/ToDo/static/js/runtime-main.3fde01ae.js"
+  }
+]);
